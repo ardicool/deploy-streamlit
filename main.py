82d7a1f717c7e4a7dfabec11b06dd9eebac7f57d
ardicool <ardi.rijjal@gmail.com>
2025-12-25 14:19:19 +0700

    komit 2

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from deploy-streamlit!")
+import numpy as np
+import pandas as pd
+import streamlit as st
+import pickle
 
+#load the model
+model = pickle.load(open('rf_model.pkl', 'rb'))
+st.title('Bank Note Authentication Prediction')
 
-if __name__ == "__main__":
-    main()
+#input features from user
+variance = st.text_input('Variance')
+skewness = st.text_input('Skewness')
+curtosis = st.text_input('Curtosis')
+entropy = st.text_input('Entropy')
+output = ''
+#prediction function
+def predict_note_authentication(variance, skewness, curtosis, entropy):
+    prediction = model.predict([[variance, skewness, curtosis, entropy]])
+    return prediction   
+#button for prediction
+if st.button('Predict'):
+    output = predict_note_authentication(variance, skewness, curtosis, entropy)
+    if output[0]==0:
+        st.success('The bank note is Authentic')
+    else:
+        st.success('The bank note is Not Authentic')
+        
